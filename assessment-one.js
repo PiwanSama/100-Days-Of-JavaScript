e44e3a13752873037e48df8f2de19558853295c2
PiwanSama <piwan.summerlee@gmail.com>
2020-07-18 17:38:49 +0300

    Assessment One
    First assessment completed

---
+++ assessment-one.js
@@ -0,0 +1,15 @@
+//Question Three
+
+console.log(0.1+0.2);
+console.log(0.1+0.2==0.3)
+
+//Question Four - Checking if a value is equal to NaN
+let a = 3, b = "seven";
+console.log(isNaN(a)); //Prints false
+console.log(isNaN(b)); //Prints true
+
+//Question Five
+console.log("0 || 1 = "+(0 || 1));
+console.log("1 || 2 = "+(1 || 2)); 
+console.log("0 && 1 = "+(0 && 1)); 
+console.log("1 && 2 = "+(1 && 2));
